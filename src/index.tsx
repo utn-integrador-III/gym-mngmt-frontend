07cb0ead3a76cb4536d45b025ea87c4263f0cac9
GenVallejo <gevallejoum@est.utn.ac.cr>
2025-07-21 18:45:50 -0600

    US-09-CreateExercise

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Login from './pages/auth/Login';
 import './styles/auth/login.css';
+import CreateExercise from './pages/trainers/CreateExercise';
+import './styles/trainers/CreateExercise.css';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -10,9 +12,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <Login />
+    <CreateExercise />
   </React.StrictMode>
 );
 
-
+ //<Login />
 reportWebVitals();
