be06595dd042be60046b8d83ced423264e823fa2
GenVallejo <gevallejoum@est.utn.ac.cr>
2025-07-22 03:29:53 -0600

    US-12-DeleteExercise

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/auth/Login';
 import CreateExercise from './pages/trainers/CreateExercise';
+import DeleteExercise from './pages/trainers/DeleteExercise';
 import './styles/auth/login.css';
 import './styles/trainers/CreateExercise.css';
 
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/createExercise" element={<CreateExercise />} />
+        <Route path="/deleteExercise" element={<DeleteExercise />} />
         {/* Add more routes as needed */}
       </Routes>
     </BrowserRouter>
