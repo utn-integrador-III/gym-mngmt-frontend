8dbfe5ab3234edbcc1b47c890b873da789d65acc
GenVallejo <gevallejoum@est.utn.ac.cr>
2025-07-22 22:32:41 -0600

    US-09-CreateRoutine and US-12-DeleteExercise

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,13 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/auth/Login';
 import CreateExercise from './pages/trainers/CreateExercise';
 import DeleteExercise from './pages/trainers/DeleteExercise';
+import CreateRoutine from './pages/trainers/CreateRoutine';
+
+
 import './styles/auth/login.css';
 import './styles/trainers/CreateExercise.css';
+import './styles/trainers/DeleteExercise.css';
+import './styles/trainers/CreateRoutine.css';
 
 function App() {
   return (
@@ -13,6 +18,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/createExercise" element={<CreateExercise />} />
         <Route path="/deleteExercise" element={<DeleteExercise />} />
+        <Route path="/createRoutine" element={<CreateRoutine />} />
         {/* Add more routes as needed */}
       </Routes>
     </BrowserRouter>
