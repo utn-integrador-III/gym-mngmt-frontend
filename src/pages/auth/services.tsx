1a431a097a07367581c0c46bd0f4fffc8edcb8eb
Susan <139490899+Suguzmanve@users.noreply.github.com>
2025-08-28 23:17:04 -0600

    Update services.tsx

--- src/pages/auth/services.tsx
+++ src/pages/auth/services.tsx
@@ -1,9 +1,2 @@
     // archivo: src/services/usersService.ts
 
-    export const usersApi = {
-    create: (formData: FormData) =>
-        fetch("http://localhost:8000/users/", {
-        method: "POST",
-        body: formData, // NO poner headers: 'Content-Type'
-        }).then(res => res.json())
-    };
